a1323b3294ba3cc7ea890ae6f92c1fbc767a300a
mike <mukulpratap@gmai.com>
2025-10-24 14:13:01 +0530

    Logout After 5 mins

--- src/Store/AuthContext.jsx
+++ src/Store/AuthContext.jsx
@@ -8,7 +8,7 @@ export const AuthContext = createContext({
   login: (token) => {},
   logout: () => {},
 });
-
+let logoutTimer;
 // Step 2️⃣ Create Provider component
 const AuthContextProvider = ({ children }) => {
   const [token, setToken] = useState(null);
@@ -16,19 +16,39 @@ const AuthContextProvider = ({ children }) => {
   // Step 3️⃣ Load stored token (if any) when app starts
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
-    if (storedToken) setToken(storedToken);
+    const storedExpiration = localStorage.getItem("tokenExpiration");
+    if (storedToken && storedExpiration) {
+      const remainingTime = storedExpiration - new Date().getTime();
+      if (remainingTime <= 0) {
+        logoutHandler();
+      } else {
+        setToken(storedToken);
+        autoLogout(remainingTime);
+      }
+    }
   }, []);
 
   // Step 4️⃣ Define login/logout functions
   const loginHandler = (token) => {
     setToken(token);
     localStorage.setItem("token", token); // persist token
+    const expirationTime = new Date().getTime() + 5 * 50 * 1000;
+    localStorage.setItem("tokenExpiration", expirationTime);
+    autoLogout(5 * 60 * 1000);
+  };
+  const autoLogout = (milliseconds) => {
+    logoutTimer = setTimeout(() => {
+      logoutHandler();
+      alert("session expired! please Login again");
+    }, milliseconds);
   };
 
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
-    alert("logout Success");
+    localStorage.removeItem("tokenExpiration");
+    if (logoutTimer) clearTimeout(logoutTimer);
+    alert("logout Successfully");
   };
 
   // Step 5️⃣ Derived state
